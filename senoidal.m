9dfb3163404829354c4656ab6d5d958c546b84fe
Alain García <daniel.gar1310@gmail.com>
2024-09-10 11:35:31 -0600

    Update and rename sin_continuo.m to senoidal.m

--- senoidal.m
+++ senoidal.m
@@ -1,8 +1,8 @@
 %Se crea el eje horizontal, en este caso con valores desde -6*pi hasta 6*pi separados 0.1 
 x = -6*pi:0.1:6*pi;
 
-%FunciÛn seno en tÈrminos de x
+%Funci√≥n seno en t√©rminos de x
 y = sin(x);
 
-%Gr·fica continua en 2D con tÌtulo y etiquetas de eje
+%Gr√°fica continua en 2D con t√≠tulo y etiquetas de eje
 plot(x,y); grid on; title('sin(x)'); xlabel('Tiempo'); ylabel('Amplitud')
