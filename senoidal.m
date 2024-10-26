a2886e3265c368dd6de9a521a521e0b84f7bf54c
Alain García <daniel.gar1310@gmail.com>
2024-10-25 19:44:10 -0600

    Update senoidal.m

--- senoidal.m
+++ senoidal.m
@@ -1,8 +1,26 @@
-%Se crea el eje horizontal, en este caso con valores desde -6*pi hasta 6*pi separados 0.1 
-x = -6*pi:0.1:6*pi;
+%Programa que grafica una señal senoidal de acuerdo a los datos que ingresa el usuario
 
-%Función seno en términos de x
-y = sin(x);
+%Pide al usuario el valor de la frecuencia, amplitud y fase de la señal de la señal. 
+%Así como el número de ciclos que se van a mostrar en la gráfica
+f = input("Ingrese la frecuencia de la señal: ");
+A = input("Ingrese la amplitud de la señal: ");
+Nc = input("Ingrese el número de ciclos que se mostrarán en la gráfica: ");
+fase = input("Ingrese la fase de la señal en grados: ");
+fase = fase*pi/180;
 
-%Gráfica continua en 2D con título y etiquetas de eje
-plot(x,y); grid on; title('sin(x)'); xlabel('Tiempo'); ylabel('Amplitud')
+%Se calcula la frecuencia de muestreo como 10 veces el valor de la frecuencia de la señal 
+fs = 10*f;
+
+%Crea el eje del tiempo utilizando los valores ingresados
+%El limite superior del eje horizontal se guarda en una variable para evitar errores
+xlim_sup =2*Nc*pi/f
+t = 0:1/fs:xlim_sup;
+
+%Función seno Asin(ft+φ)
+y = A*sin(f*t+fase);
+
+%Gráfica continua en pantalla completa con título, etiquetas de eje y límites de los ejes
+figure('units','normalized','outerposition',[0 0 1 1])
+plot(t,y); grid on; 
+title('sin(x)'); xlabel('Tiempo'); ylabel('Amplitud'); 
+xlim([0 xlim_sup]); ylim([-A A]);
